f0a6fc3df3955d4995bb7d5ff9364ece199e1cdf
EW <96669967+bigbrrrr@users.noreply.github.com>
2024-05-15 12:59:01 -0500

    Change script to test db tracking

--- scripts/restartserver.sh
+++ scripts/restartserver.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# cp /home/ec2-user/BearSchedule/instance/site.db /home/ec2-user/BearSchedule/instance/site2.db
-# sudo rm /home/ec2-user/BearSchedule/instance/site.db
-# mv /home/ec2-user/BearSchedule/instance/site2.db /home/ec2-user/BearSchedule/instance/site.db
 sudo systemctl restart nginx
 sudo systemctl restart bear
