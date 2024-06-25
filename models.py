e900bc65ac8ceb2b93b6fec8c604979131aabf89
EW <96669967+bigbrrrr@users.noreply.github.com>
2024-06-25 00:40:04 -0500

    New features and maintenance
    - spam filter for shifts
    - split '/sms' endpoint
    - test env variable
    - chatbot is less dumb

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 import json
+import os
 from werkzeug.security import generate_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
@@ -13,7 +14,10 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
-    phone = db.Column(db.Integer, unique=True)
+    if os.environ.get('ENV') == 'dev':
+        phone = db.Column(db.Integer, unique=False)
+    else:
+        phone = db.Column(db.Integer, unique=True)
     def as_dict(self):
        return {'id': self.id, 'username': self.username, 'phone': self.phone}
 
