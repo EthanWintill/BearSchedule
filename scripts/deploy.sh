8ccab7bf200ba71241a18139b5e29b3c5b013790
EW <96669967+bigbrrrr@users.noreply.github.com>
2025-09-09 22:43:08 -0500

    deploy script test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-cd /root/BearSchedule
+cd /var/www/BearSchedule
 source .venv/bin/activate
+git pull
 pip install -r requirements.txt
 sudo systemctl restart nginx
 sudo systemctl restart bear
